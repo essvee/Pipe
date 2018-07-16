5ab9d34457792d5b53965856f6fc2e2da215a05c
essvee <s.vincent@nhm.ac.uk>
2018-07-16 18:44:24 +0100

    Moves IO to util.write_out

--- pipe/logic.py
+++ pipe/logic.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import csv
 from pipe.harvest import gapi
+from pipe.utils import write_out
 
-# Get email titles and metadata from gmail inbox
+# Get email titles and metadata from gmail inbox + write to file
 result_list = gapi.main()
+fields = result_list[0].keys()
+write_out.dict_to_csv(result_list, fields, 'gapi_results.csv')
 
-with open('gapi_results.csv', 'w', newline='\n') as csvfile:
 
-    fieldnames = ['title', 'label', 'format', 'bib_details', 'snippet']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, dialect='excel')
-    writer.writeheader()
-    writer.writerows(result_list)
-
-# Send just titles for each to crossref
+# TODO - Send just titles for each to crossref
 # Time how long it takes and the fuzzy match score
 # export to csv
 
-# Send titles + associated metadata for each to crossref
+#  TODO - Send titles + associated metadata for each to crossref
 # Time how long it takes and the fuzzy match score
 # write to csv
 
