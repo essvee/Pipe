00fe8c070e6aa0a599472da72f53837c2ba03f24
essvee <s.vincent@nhm.ac.uk>
2018-07-16 18:43:48 +0100

    Adds write-out util

---
+++ pipe/utils/write_out.py
@@ -0,0 +1,8 @@
+import csv
+
+
+def dict_to_csv(dict_data, headers, filename):
+    with open(filename, 'w', newline='\n') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=headers, dialect='excel')
+        writer.writeheader()
+        writer.writerows(dict_data)
