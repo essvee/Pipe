9a27daec4198def1f710ef096e76e2b485f195a2
essvee <s.vincent@nhm.ac.uk>
2018-08-08 21:02:26 +0100

    adds test file

---
+++ tests/test_harvest.py
@@ -0,0 +1,16 @@
+import unittest
+import googleapiclient
+from pipe.harvest.gapi import Gapi
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_get_credentials(self):
+        gapi = Gapi()
+        result = gapi.get_credentials()
+        self.assertTrue(isinstance(result, googleapiclient.discovery.Resource))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
