9ea81e3a584dd6f7f598e6a56e78715a8d98bad6
essvee <s.vincent@nhm.ac.uk>
2018-08-18 20:20:08 +0100

    adds tests

--- tests/test_harvest.py
+++ tests/test_harvest.py
@@ -1,4 +1,6 @@
 import unittest
+from datetime import date
+
 import googleapiclient
 import pipe.src.gapi
 
@@ -9,10 +11,28 @@ class MyTestCase(unittest.TestCase):
         myGapi = pipe.src.gapi.Gapi()
         self.assertTrue(isinstance(myGapi, pipe.src.gapi.Gapi))
 
-    def test_fields(self):
+    def test_get_credentials(self):
         myGapi = pipe.src.gapi.Gapi()
         result = myGapi.get_credentials()
         self.assertTrue(isinstance(result, googleapiclient.discovery.Resource))
+    #
+    # def test_get_unread_email_ids(self):
+    #     myGapi = pipe.src.gapi.Gapi()
+    #     service = myGapi.get_credentials()
+    #     unread_emails = myGapi.list_unread_emails(service)
+    #     self.assertTrue(unread_emails[0]['id'] == '1654cbffaf5c56a6')
+
+    # def test_get_emails(self):
+    #     myGapi = pipe.src.gapi.Gapi()
+    #     service = myGapi.get_credentials()
+    #     email_id = '1654cbffaf5c56a6'
+    #     email = myGapi.get_email(service, email_id)
+    #     self.assertTrue(email['id'] == '1654cbffaf5c56a6')
+
+    def test_get_email_creation(self):
+        myGapi = pipe.src.gapi.Gapi()
+        result = myGapi.main()
+        self.assertTrue(result[0].harvested_date == date.today())
 
 
 
