e0f479b3b845ad3a85794bbdf888f04b41e0ebf4
essvee <s.vincent@nhm.ac.uk>
2018-08-20 17:30:13 +0100

    adds default values + string-cleaning function

--- pipe/src/message.py
+++ pipe/src/message.py
@@ -3,12 +3,19 @@ from dataclasses import dataclass
 
 @dataclass
 class Message:
-    title: str
     citation_format: str
+    title: str
     bib_data: str
     snippet: str
     m_author: str
     m_pub_title: str
-    m_pub_year: int
+    m_pub_year: str
+    label: str
+    email_id: int = None
     message_id: int = None
 
+    # Returns field values as dict
+    def get_values(self):
+        return {'citation_format': self.citation_format, 'title': self.title, 'bib_data': self.bib_data,
+                'snippet': self.snippet, 'm_author': self.m_author, 'm_pub_title': self.m_pub_title,
+                'm_pub_year': self.m_pub_year, 'label': self.label, 'email_id': self.email_id, 'message_id': self.message_id}
