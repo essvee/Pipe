9f7d8d926ab6b54c90b2dbfe90bf2e3e7565aacc
essvee <s.vincent@nhm.ac.uk>
2018-08-20 13:25:49 +0100

    refactors Message as dataclass

--- pipe/src/message.py
+++ pipe/src/message.py
@@ -1,13 +1,14 @@
-class Message(object):
-    def __init__(self, title, citation_format, bib_data, snippet, m_author, m_pub_title, m_pub_year):
-        self.citation_format = citation_format
-        self.title = title
-        self.bib_data = bib_data
-        self.snippet = snippet
-        self.m_author = m_author
-        self.m_pub_title = m_pub_title
-        self.m_pub_year = m_pub_year
+from dataclasses import dataclass
 
-    def extract_bib_data(self):
-        # TODO implement this method
-        pass
+
+@dataclass
+class Message:
+    title: str
+    citation_format: str
+    bib_data: str
+    snippet: str
+    m_author: str
+    m_pub_title: str
+    m_pub_year: int
+    message_id: int = None
+
