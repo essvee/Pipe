2e382ba07f39b199c8771dbd3eef04220471cc1d
essvee <s.vincent@nhm.ac.uk>
2018-08-20 17:30:32 +0100

    writes objects to csv

--- pipe/src/util.py
+++ pipe/src/util.py
@@ -3,9 +3,17 @@ import csv
 
 class Util:
 
+    # Writes result set to csv. Takes a list of dictionaries: one per message
     @staticmethod
-    def dict_to_csv(dict_data, headers, filename):
+    def write_object_to_csv(list_dict_data, filename):
         with open(filename, 'w', newline='\n') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=headers, dialect='excel')
+
+            headers = list(list_dict_data[0].get_values().keys())
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
             writer.writeheader()
-            writer.writerows(dict_data)
+
+            try:
+                for i in list_dict_data:
+                    writer.writerow(i.get_values())
+            except AttributeError as error:
+                print(error.__repr__())
