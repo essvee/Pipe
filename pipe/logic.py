128ac633c42739659402a3a98591d909ba0717a1
essvee <s.vincent@nhm.ac.uk>
2018-08-25 15:34:35 +0100

    retrieves unidentified messages from db

--- pipe/logic.py
+++ pipe/logic.py
@@ -2,24 +2,44 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
-from pipe.src import harvest_gmail, util
+from pipe.src import harvest_gmail, util, message
 from pipe.src.identify_crossref import IdentifyCrossRef
 from pipe.src.util import Util
 
 a = datetime.now()
 u = Util()
 
-# Get Message objects from Gmail
-messages = harvest_gmail.HarvestGmail().main()
+# todo - uncomment this when crossref module working
+# # Get Message objects from Gmail
+# messages = harvest_gmail.HarvestGmail().main()
+#
+# # Write messages to database
+# message_sql = """INSERT INTO message_store (email_id, title, snippet, m_author, m_pub_title, m_pub_year, sent_date,
+# harvested_date, source, id_status, label_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+#
+# u.write_new(message_sql, messages)
 
-# Write messages to database
-message_sql = """INSERT INTO message_store (email_id, title, snippet, m_author, m_pub_title, m_pub_year, sent_date,
-harvested_date, source, id_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+# Get all unidentified messages from message_store
+unidentified_sql = "SELECT * FROM message_store WHERE id_status = False"
+cursor = u.query_db(unidentified_sql)
+mystery_messages = cursor.fetchall()
 
-u.write_new(message_sql, messages)
+# Turn each result back into Message obj
+checklist = [message.Message(message_id=i[0],
+                             email_id=i[1],
+                             title=i[2],
+                             snippet=i[3],
+                             m_author=i[4],
+                             m_pub_title=i[5],
+                             m_pub_year=i[6],
+                             sent_date=i[7],
+                             harvested_date=i[8],
+                             source=i[9],
+                             identification_status=i[10],
+                             label=i[11]
+                             ) for i in mystery_messages]
 
-
-for n in messages:
+for n in checklist:
     print(n)
 
 # for n in email_list:
@@ -33,28 +53,4 @@ b = datetime.now()
 c = b - a
 print(f"Runtime: {c}")
 
-
-
-# w = util.Util()
-#
-# for n in result_list:
-#     for i in n.messages:
-#         message_list.append(i)
-#
-# w.write_object_to_csv(list_dict_data=message_list, filename='gapi_results_messages.csv')
-
-# cr_i = CrossRefIdentifier(result_list)
-#
-# cr_i_result_list = cr_i.get_crossref_match()
-#
-# for i in cr_i_result_list:
-#     print(i)
-
-# TODO - Send just titles for each to crossref
-# Time how long it takes and the fuzzy match score
-# export to csv
-
-#  TODO - Send titles + associated metadata for each to crossref
-# Time how long it takes and the fuzzy match score
-# write to csv
 
