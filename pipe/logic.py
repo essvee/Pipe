6de90695dc09315bd8bd8b14e7473ef01186e295
essvee <s.vincent@nhm.ac.uk>
2018-09-01 19:27:10 +0100

    Cross-references against NHM library holdings

--- pipe/logic.py
+++ pipe/logic.py
@@ -20,8 +20,10 @@ if messages:
 
     u.write_new_objects(message_sql, messages)
 
+else:
     # Get all unidentified messages from message_store
-    unidentified_sql = "SELECT * FROM message_store WHERE id_status = False"
+    unidentified_sql = "SELECT * FROM message_store WHERE id_status = False AND (last_crossref_run IS NULL " \
+                       "OR last_crossref_run < ADDDATE(DATE(NOW()), INTERVAL -1 MONTH))"
     cursor = u.query_db(unidentified_sql)
     mystery_messages = cursor.fetchall()
 
@@ -41,14 +43,17 @@ if messages:
                                  ) for i in mystery_messages]
 
     cr_i = IdentifyCrossRef(checklist)
-    identified_citations = cr_i.get_crossref_match()
+    identified_citations, unidentified_citations = cr_i.get_crossref_match()
     found_messages = []
     for x, y in identified_citations.items():
         for i in y.message_ids:
             found_messages.append((x, i))
 
-    update_msg = """UPDATE message_store SET doi = %s, id_status = True WHERE message_id = %s"""
-    u.write_new_normals(update_msg, found_messages)
+    update_msg_found = """UPDATE message_store SET doi = %s, id_status = True WHERE message_id = %s"""
+    u.write_new_normals(update_msg_found, found_messages)
+
+    update_msg_unidentified = """UPDATE message_store SET last_crossref_run = %s WHERE message_id = %s"""
+    u.write_new_normals(update_msg_unidentified, unidentified_citations)
 
     # Write messages to citation database
     citation_sql = """INSERT INTO citation_store (author, doi, title, type, issued_date, subject, pub_title, pub_publisher,
@@ -57,6 +62,10 @@ if messages:
 
     verified_citations = []
     u.write_new_objects(citation_sql, identified_citations.values())
+
+    # Update nhm_pub by checking against table of subscribed journals
+    nhm_pub_sql = 'UPDATE citation_store SET nhm_sub = 1 WHERE issn IN (SELECT issn from nhm_pubs)'
+    u.query_db(unidentified_sql)
 
 b = datetime.now()
 c = b - a
