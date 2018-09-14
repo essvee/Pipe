b38f73966b757c97a1545c717e7a0e8b6cb94042
essvee <s.vincent@nhm.ac.uk>
2018-09-14 21:14:08 +0100

    retrieves bibliometric data

--- pipe/src/dimensions.py
+++ pipe/src/dimensions.py
@@ -1,8 +1,44 @@
-class Dimensions:
-    def __init__(self, citations):
-        self.citations = citations
-        self.dimensions_url = "fill this in"
+from datetime import date
+from requests import HTTPError
+import requests
+import time
+from pipe.src.util import Util as u
 
-    def get_bibliometrics(self):
-        # TODO implement this method
-        pass
+class Dimensions:
+    def __init__(self, citation_dois):
+        self.citation_dois = citation_dois
+        self.date_retrieved = date.today().strftime('%Y-%m-%d')
+
+
+    def get_citations(self):
+        insert_sql = "INSERT INTO bibliometrics(times_cited, recent_citations, retrieved_date, relative_citation_ratio," \
+                     " field_citation_ratio, doi) VALUES(%s, %s, %s, %s, %s, %s)"
+
+        results = []
+
+        # For each DOI in list, retrieve citation metrics
+        for doi in self.citation_dois:
+            # Throttle query rate to comply with API terms of use
+            time.sleep(1)
+            url = f"https://metrics-api.dimensions.ai/doi/{doi[0]}"
+            print(f"URL: {url}")
+            try:
+                r = requests.get(url)
+                r.raise_for_status()
+
+                results.append((r.json()['times_cited'] or 0,
+                                r.json()['recent_citations'] or 0,
+                                self.date_retrieved,
+                                r.json()['relative_citation_ratio'] or 0,
+                                r.json()['field_citation_ratio'] or 0,
+                                doi))
+            # Skip over DOIs which aren't found
+            except HTTPError:
+                print(f"No results for {doi}")
+                continue
+            except ConnectionError:
+                print(f"Connection error for {doi}")
+                continue
+
+        # Return
+        return insert_sql, results
