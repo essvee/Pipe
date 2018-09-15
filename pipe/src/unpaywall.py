ca155901c81439a9b1de2ca32b564376fa256e0e
essvee <s.vincent@nhm.ac.uk>
2018-09-15 16:02:34 +0100

    Implements access data retrieval

--- pipe/src/unpaywall.py
+++ pipe/src/unpaywall.py
@@ -1,7 +1,55 @@
-class Unpaywall:
-    def __init__(self, citations):
-        self.citations = citations
+from datetime import date
+from requests import HTTPError
+import requests
 
-    def get_oa_details(self):
-        # TODO implement this method
-        pass
+
+class Unpaywall:
+    def __init__(self, citation_dois):
+        self.citation_dois = citation_dois
+        self.date_retrieved = date.today().strftime('%Y-%m-%d')
+
+    def get_access_data(self):
+        insert_sql = "INSERT INTO open_access(best_oa_url, updated_date, retrieved_date, " \
+                     "pdf_url, is_oa, doi, doi_url, host_type, version) " \
+                     "VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+        results = []
+
+        # For each DOI in list, retrieve citation metrics
+        for doi in self.citation_dois:
+            url = f"https://api.unpaywall.org/v2/{doi[0]}?email=s.vincent@nhm.ac.uk"
+
+            try:
+                r = requests.get(url)
+                r.raise_for_status()
+
+                if r.json()['is_oa'] is True:
+                    best_oa_location = r.json()['best_oa_location']
+                    results.append((best_oa_location['url'] or None,
+                                    best_oa_location['updated'][:10] or r.json()['updated'][:10],
+                                    self.date_retrieved,
+                                    best_oa_location['url_for_pdf'] or None,
+                                    r.json()['is_oa'],
+                                    doi[0],
+                                    r.json()['doi_url'] or None,
+                                    best_oa_location['host_type'] or None,
+                                    best_oa_location['version'] or None
+                                    ))
+                else:
+                    results.append((None,
+                                    r.json()['updated'][:10] or None,
+                                    self.date_retrieved,
+                                    None,
+                                    r.json()['is_oa'],
+                                    doi[0],
+                                    r.json()['doi_url'] or None,
+                                    None,
+                                    None
+                                    ))
+
+            # Skip over DOIs which aren't found
+            except HTTPError:
+                continue
+
+        # Return
+        return insert_sql, results
