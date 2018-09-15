832c4b112942cfb2810d8a4d833662bcafc99fca
essvee <s.vincent@nhm.ac.uk>
2018-09-15 16:04:13 +0100

    Adds model retrieval

--- pipe/src/classifier.py
+++ pipe/src/classifier.py
@@ -1,8 +1,17 @@
+import dill as pickle
+
+
 class Classifier:
     def __init__(self, citations):
         self.citations = citations
-        self.model = None
+        self.model = self.load_model()
+
+    def load_model(self):
+        with open('model_forest.pk', 'rb') as f:
+            loaded_forest = pickle.load(f)
+        return loaded_forest
 
     def classify(self):
         # TODO implement this method
         pass
+
