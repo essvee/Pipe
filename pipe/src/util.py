0ed8ac0e8eca5ab9abe16976c1299a2164fad41b
essvee <s.vincent@nhm.ac.uk>
2018-09-24 21:18:47 +0100

    Adds csv write function

--- pipe/src/util.py
+++ pipe/src/util.py
@@ -68,3 +68,41 @@ class Util:
                 return cursor
             except pymysql.Error as e:
                 print(e)
+
+    def update_repo(self):
+        # Export citation-level data
+        sql_citation_level = "SELECT * FROM vw_citation_level"
+        citation_header =  ['authors', 'doi', 'title', 'classification_id', 'type', 'issued_date', 'subject',
+                            'journal_title', 'publisher', 'issn', 'isbn', 'issue', 'volume', 'page', 'nhm_sub',
+                            'oa_id', 'best_oa_url', 'updated_date', 'pdf_url', 'is_oa', 'doi_url', 'host_type',
+                            'version', 'retrieved_date']
+
+        cursor = self.query_db(sql_citation_level)
+        self.write_object_to_csv(cursor.fetchall(), citation_header, 'citation_export.csv')
+
+        # Export message-level data
+        sql_message_level = "SELECT * FROM vw_message_level"
+        message_header = ['doi', 'label_name', 'date_sent']
+
+        cursor = self.query_db(sql_message_level)
+        self.write_object_to_csv(cursor.fetchall(), message_header, 'message_export.csv')
+
+        # Export impact data
+        sql_impact = "SELECT * FROM vw_impact"
+        impact_header = ['doi', 'times_cited', 'retrieved_date']
+
+        cursor = self.query_db(sql_impact)
+        self.write_object_to_csv(cursor.fetchall(), impact_header, 'impact_export.csv')
+
+
+    # Writes result set to csv. Takes a list of dictionaries: one per message
+    @staticmethod
+    def write_object_to_csv(list_data, headers, filename):
+        with open(filename, 'w', newline='\n') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(headers)
+
+            try:
+                writer.writerows(list_data)
+            except AttributeError as error:
+                print(error.__repr__())
