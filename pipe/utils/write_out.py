b2e5a62534d86c7ffa63b36d3c1b431c50f00c8a
essvee <s.vincent@nhm.ac.uk>
2018-10-02 15:41:36 +0100

    updates data

--- pipe/utils/write_out.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-
-def dict_to_csv(dict_data, headers, filename):
-    with open(filename, 'w', newline='\n') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=headers, dialect='excel')
-        writer.writeheader()
-        writer.writerows(dict_data)
