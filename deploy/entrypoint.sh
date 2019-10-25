8d4598d0fc138dbfb4b8ff5c717fd7dbe92b7d5e
Alice Butcher <alycejenni@gmail.com>
2019-10-25 09:14:59 +0100

    Import epitator in entrypoint if it doesn't exist

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -12,13 +12,15 @@ cd /opt/app || exit
 
 mysql --host=$DATABASE_HOST --user=root --password=pass --database=pipe_db --protocol=tcp < deploy/pipe_schema.sql
 
-
+if [ ! -f "/root/.epitator.sqlitedb" ]; then
+  # this doesn't work if you put it in the Dockerfile but
+  # it takes ages so we only want to do it once
+  echo "Importing epitator"
+  python -m epitator.importers.import_all
+fi
 
 if [ -f "pipe/data/gmail-credentials.json" ]; then
   python -m pipe.src.runme
 else
-  # this doesn't work if you put it in the Dockerfile but
-  # it takes ages so we only want to do it once
-  python -m epitator.importers.import_all
   echo "Now run:\n\tdocker run -it pipe_backend python /opt/app/deploy/auth.py --noauth_local_webserver"
 fi
