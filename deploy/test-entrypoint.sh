15d80f90791091b3cf57db3c2e206fb115520993
Alice Butcher <alycejenni@gmail.com>
2020-02-20 12:18:04 +0000

    Adding .travis.yml and test script
    Tests can be run with `docker-compose run backend sh deploy/test-entrypoint.sh`

---
+++ deploy/test-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for MySQL..."
+
+while ! mysqladmin ping -h "$TEST_DATABASE_HOST" --silent > /dev/null 2> /dev/null; do
+    sleep 1
+done
+
+echo "MySQL started"
+
+cd /opt/app || exit
+
+python -m pytest
